5b852b97f45dafb2b77d901bacea14a7a9af19bd
jiroquijano <jiroquijano@gmail.com>
2020-07-09 17:08:19 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import './styles/App.css';
+import PostsFeedPage from './components/PostsFeedPage';
+import PostViewPage from './components/PostViewPage';
+import Header from './components/Header';
+import BlogContext from './context/blog-context';
+import postReducer from './reducers/postReducers';
 
 function App() {
+
+  const [posts,dispatch] = useReducer(postReducer, []);
+  
+  useEffect(()=>{
+    const initial = [{title:'jiroooo', content:'sample content', keywords:'keywooooord'}];
+    localStorage.setItem("posts", JSON.stringify(initial));
+    const storedPosts = JSON.parse(localStorage.getItem("posts")) || [{title:'jiropost', content: 'sample', keywords:'keyword'}];
+    dispatch({
+      type: 'POPULATE_POSTS',
+      posts: storedPosts
+    });
+
+    dispatch({
+      type: 'ADD_POST',
+      post: {title:'bonna', content: 'ganda', keywords:'prehti'}
+    });
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BlogContext.Provider value={{posts,dispatch}}>
+      <Header/>
+      <BrowserRouter>
+        <Route path='/' exact component={PostsFeedPage}/>
+        
+        <Route path='/view/:id' test={'hello'} component={PostViewPage}/>
+      </BrowserRouter>
+    </BlogContext.Provider>
   );
 }
 
