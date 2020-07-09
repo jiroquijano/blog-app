21b01c47aeea65fb9c1e482b7f09fa918f2927d3
jiroquijano <jiroquijano@gmail.com>
2020-07-09 20:49:20 +0800

    added modal warning when adding note with empty title or content

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,34 @@
-import React, {useContext} from 'react';
+import React, {useContext,useState} from 'react';
 import {Link} from 'react-router-dom';
 import BlogContext from '../context/blog-context';
+import Modal from 'react-modal';
 
 const Post = ({post}) =>{
     const {dispatch} = useContext(BlogContext);
-    
+    const [isModalOpen, setModalOpen] = useState(false);
+
     const handleDelete = () =>{
         dispatch({
             type: 'REMOVE_POST',
             id: post.id
-        })
+        });
+        setModalOpen(false);
     };
 
     return (
         <div>
             <h2>{post.title}</h2>
             <Link to={`/view/${post.id}`}>View</Link>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={()=>setModalOpen(true)}>Delete</button>
+            <Modal 
+                isOpen={isModalOpen}
+                onRequestClose={()=>setModalOpen(false)}
+                ariaHideApp={false}
+            >
+                <h1>Delete post?</h1>
+                <button onClick={()=>setModalOpen(false)}>Cancel</button>
+                <button onClick={handleDelete}>Confirm</button>
+            </Modal>
         </div>
     )
 };
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,20 +1,38 @@
 import React, {useState, useContext} from 'react';
 import BlogContext from '../context/blog-context';
+import {useHistory} from 'react-router-dom';
+import Modal from 'react-modal';
 
 const PostForm = ({actionType='ADD_POST', post}) => {
     const [title, setTitle] = useState(post? post.title: '');
     const [content, setContent] = useState(post? post.content:'');
     const [keywords, setKeywords] = useState(post? post.keywords:'');
+    const [isModalOpen, setModalOpen] = useState(false);
+    const history = useHistory();
     const {dispatch} = useContext(BlogContext);
 
     return (
+        <>
+        <Modal 
+            isOpen={isModalOpen}
+            ariaHideApp={false}
+            onRequestClose={()=>setModalOpen(false)}
+        >
+            <h1>Post Title and Post content should not be Empty!</h1>
+            <button onClick={()=>setModalOpen(false)}>Okay, sorry</button>
+        </Modal>
         <form 
             onSubmit={(e)=>{
                 e.preventDefault();
-                dispatch({
-                    type: actionType,
-                    post: {title,content,keywords}
-                })
+                if(!title || !content){ 
+                    setModalOpen(true)
+                } else {
+                    dispatch({
+                        type: actionType,
+                        post: {title,content,keywords}
+                    });
+                    history.push('/');
+                }
             }}
         >
             <input
@@ -42,6 +60,7 @@ const PostForm = ({actionType='ADD_POST', post}) => {
             />
             <button>Submit!</button>
         </form>
+        </>
     )
 }
 
